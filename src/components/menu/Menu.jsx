29e37d7dfc1a5aad3d1dbdcac8f14583c7872465
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-09-08 19:04:33 +0300

    adding menu and home

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import "./menu.scss"
+import React from "react";
+import "./menu.scss";
+import { Link } from "react-router-dom";
+import { menu } from "../../data";
 const Menu = () => {
   return (
-    <div className='menu'>
-      menu
-    </div>
-  )
-}
+    <div className="menu">
+      {
+        menu.map((item)=> (
+            <div className="item" key={item.id}>
+              <span className="title">{item.title}</span>
+              {
+                item.listItems.map((listItem)=>(
+              <Link to={listItem.url} className="listItem" key={listItem.id}>
+                <img src={listItem.icon} alt="main" />
+                <span className="listItem Title">{listItem.title}</span>
+              </Link>
+                ))
+              }
+            </div>
 
-export default Menu
+
+          ))
+      } 
+    </div>
+  );
+};
+
+export default Menu;
